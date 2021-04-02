299af19b08194f3eef718e5bd8afb00242199099
herren k <hyk@herren.co.kr>
2021-04-02 14:41:47 +0900

    add codes

--- PoisonCore/src/main/java/com/poisionrose/poisoncore/CoreBase.kt
+++ PoisonCore/src/main/java/com/poisionrose/poisoncore/CoreBase.kt
@@ -1,14 +1,24 @@
 package com.poisionrose.poisoncore
 
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class CoreBase {
+
+    fun getName() : String {
+        return "name!"
+    }
 
     companion object {
 
         fun initialize() : String {
             Log.e("test", "initialize in core")
+            Log.e("test", "it good!")
             return "ok!"
         }
     }
+}
+
+fun AppCompatActivity.getCategory() : String {
+    return this.localClassName
 }
